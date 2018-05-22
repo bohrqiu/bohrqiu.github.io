1a2c6353c31528e00d3b9e5518c69566294c5537
bohr.qiu <bohr.qiu@gmail.com>
2018-05-22 17:04:39 +0800

    修改脚本

--- gitpush.sh
+++ gitpush.sh
@@ -1,6 +1,5 @@
 git add .
-echo "输入git备注信息:"
-read -p "message:" -t 30 commitMsg
+read -p "输入git备注信息:" -t 30 commitMsg
 if [ "$commitMsg" = "" ]; then
   echo "new host name is empty, not set new hostname."
   exit 0
